f6eef23ea9a8c2195f887134c6df142a28f49732
FlavioGP <flavio.gpinheiro@hotmail.com>
2022-06-22 11:10:49 -0300

    desafio contador Impulso JavaScript

---
+++ Impulso-JavaScript/contador/assets/js/script.js
@@ -0,0 +1,56 @@
+class Contador{
+    constructor(){
+        this.contador = document.querySelector('.contador');
+        this.init();
+    }
+
+    init(){
+
+        this.events();
+    }
+
+    events(){
+        this.contador.addEventListener('click', e => {
+            const span = this.contador.querySelector('.number');
+            const el = e.target;
+            this.checkInput(span);
+            this.score(el, span);
+        });
+    }
+
+    score(el, span){
+
+        if(el.classList.contains('increment')){
+            console.log(span); 
+            span.innerHTML++;
+        };
+        if(el.classList.contains('decrement')){
+            console.log(span); 
+             span.innerHTML--;
+            };
+    }
+
+    checkInput(span){
+        const check = document.querySelector('.check');
+        const checkBoxes =  check.querySelectorAll('input[type="checkbox"]');
+            
+
+            for(let input of checkBoxes){
+                if(input.classList.contains('negative-number') && !input.checked){  
+                    if (span.innerText <= 0) {
+                        span.innerText = 0;
+                    };
+                };
+
+                if(input.classList.contains('change-color') && input.checked){  
+                    if(span.innerText == 0) span.style.color = 'black';
+                    if(span.innerText > 0) span.style.color = 'green';
+                    if(span.innerText < 0) span.style.color = 'red';
+                }   
+            }
+    }
+
+}
+
+
+const contador = new Contador();
