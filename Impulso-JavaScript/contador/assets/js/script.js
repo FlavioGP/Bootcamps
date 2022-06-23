93995fc6d36f94a1b6f4126042ccef2808f07ec3
FlavioGP <flavio.gpinheiro@hotmail.com>
2022-06-23 09:19:18 -0300

    Script funcional

--- Impulso-JavaScript/contador/assets/js/script.js
+++ Impulso-JavaScript/contador/assets/js/script.js
@@ -1,11 +1,12 @@
+
 class Contador{
     constructor(){
         this.contador = document.querySelector('.contador');
+        this.display = 0;
         this.init();
     }
 
     init(){
-
         this.events();
     }
 
@@ -13,35 +14,51 @@ class Contador{
         this.contador.addEventListener('click', e => {
             const span = this.contador.querySelector('.number');
             const el = e.target;
-            this.checkInput(span);
             this.score(el, span);
+            console.log(this.display);
         });
     }
 
     score(el, span){
 
-        if(el.classList.contains('increment')) span.innerHTML++;
-        if(el.classList.contains('decrement')) span.innerHTML--;
-
+        if(el.classList.contains('increment')){ 
+            this.display++;
+        }
+        if(el.classList.contains('decrement')) {
+            if(!this.negatives()) return;
+            this.display--;
+        }
+        this.changeColor(span);
+        span.innerText = `${this.display}`;
     }
 
-    checkInput(span){
+    negatives(){
+        const check = document.querySelector('.check');
+        const checkBoxes =  check.querySelectorAll('input[type="checkbox"]');
+        let flag = true;
+
+        for(let input of checkBoxes){
+            if(input.classList.contains('negative-number') && !input.checked){
+                if (this.display <= 0) {
+                    this.display = 0;
+                    return flag = false;
+                }
+            };     
+        };
+        return flag;
+    }
+
+    changeColor(span){
         const check = document.querySelector('.check');
         const checkBoxes =  check.querySelectorAll('input[type="checkbox"]');
 
             for(let input of checkBoxes){
 
-                if(input.classList.contains('negative-number') && !input.checked){  
-                    if (span.innerText <= 0) span.innerText = 0;
-                };
-
                 if(input.classList.contains('change-color') && input.checked){  
-                    if(span.innerText > 0) span.style.color = 'green';
-                    if(span.innerText < 0) span.style.color = 'red';
-                }else{
-                    span.style.color = 'black';
+                    if(this.display == 0) span.style.color = 'black';
+                    if(this.display >= 1) span.style.color = 'green';
+                    if(this.display <= -1) span.style.color = 'red';
                 }
-
             }
     }
 
