691a59383104468dcafb062892de1c0ebe923d19
FlavioGP <flavio.gpinheiro@hotmail.com>
2022-06-27 11:25:08 -0300

    inserindo verificçao de palavras

--- Impulso-JavaScript/variaveis-e-tipos/palindromo.js
+++ Impulso-JavaScript/variaveis-e-tipos/palindromo.js
@@ -1,9 +1,12 @@
 function palindromo(string) {
     if (!string) return console.log('Insira uma palavra/frase');
+    string = string.toLowerCase();
+    string = string.replaceAll(' ', '');
+
 
     for (let i = 0; i < string.length / 2; i++) {
         if (string[i] !== string[string.length - 1 - i]) {
-            
+
             return console.log(`${string} não é um palindromo`);
         }
 
@@ -13,4 +16,22 @@ function palindromo(string) {
 }
 
 
-palindromo('ama');
+function palindromo2(string) {
+    if (!string) return console.log('Insira uma palavra/frase');
+    string = string.toLowerCase();
+    string = string.replaceAll(' ', '');
+
+    return string.split('').reverse().join('') === string ?
+        console.log(`${string} é um palindromo`) :
+        console.log(`${string} não é um palindromo`);
+}
+
+
+
+palindromo('Acuda cadela da Leda caduca');
+palindromo2('A dama admirou o rim da amada');
+
+// A base do teto desaba
+// A cara rajada da jararaca
+// Acuda cadela da Leda caduca
+// A dama admirou o rim da amada
